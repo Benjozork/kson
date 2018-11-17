6c00639d59ffc6ef30e52aa0f860f0ff304d1d87
benjozork <benjozorkfr@gmail.com>
2018-11-16 19:53:19 -0500

    temporary tests for writer

--- src/main/kotlin/me/benjozork/kson/Kson.kt
+++ src/main/kotlin/me/benjozork/kson/Kson.kt
@@ -1,7 +1,19 @@
 package me.benjozork.kson
 
-import me.benjozork.kson.parser.ParserTest
+import me.benjozork.kson.parser.JsonObjectParser
+import me.benjozork.kson.parser.internal.JsonReader
+
+import me.benjozork.kson.writer.JsonObjectWriter
+import me.benjozork.kson.writer.JsonWriter
 
 fun main(args: Array<String>) {
-    ParserTest.parse()
+
+    val bd = StringBuilder()
+
+    val writer = JsonWriter(bd)
+
+    JsonObjectWriter.print(writer,  JsonObjectParser.read(JsonReader("{\"test\": \"test2\", \"array\": [\"val1\", \"val2\", \"val3\"]}")))
+
+    println(bd.toString())
+
 }
